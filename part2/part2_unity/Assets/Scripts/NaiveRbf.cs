186c5e0078b5d6968034fa56d9f1ace3ac266c15
pierre simon <sim.pierre91@gmail.com>
2018-07-20 22:45:30 +0200

    fix unity | implem rbf regression

--- part2/part2_unity/Assets/Scripts/NaiveRbf.cs
+++ part2/part2_unity/Assets/Scripts/NaiveRbf.cs
@@ -10,7 +10,9 @@ public class NaiveRbf : MonoBehaviour {
 	private Transform _dots;
 	public GameObject BlueDot;
 	public GameObject RedDot;
+	public GameObject PlanDot;
 	public float Gamma;
+	public bool Regression;
 	
 	[DllImport("Unity_dll")]
 	private static extern IntPtr trainNaiveRBF(float[] inputs, int inputsRows, int inputsCols, float[] outputs, int outputsRows, int outputsCols, float gamma);
@@ -31,38 +33,23 @@ public class NaiveRbf : MonoBehaviour {
 			dotsTargets.Add(item:  _dots.GetChild(i).CompareTag(tag: "red") ? 1.0f : -1.0f);
 		}
 		
-		Debug.Log("dotsInputs shape = " + dotsInputs.Count);
-		var str = "{ ";
-		foreach (var v in dotsInputs)
-		{
-			str += v + "f, ";
-		}
-		str += "}";
-		Debug.Log(str);
-		str = "{ ";
-		Debug.Log("dotsTargets shape = " + dotsTargets.Count);
-		foreach (var v in dotsTargets)
-		{
-			str += v + "f, ";
-		}
-		str += "}";
-		Debug.Log(str);
-		
-		
-		//var phi = findPhi(dotsInputs.ToArray(), dotsInputs.Count / 2, 2, 0.1f);
 		var weights = trainNaiveRBF(dotsInputs.ToArray(), dotsInputs.Count / 2, 2, dotsTargets.ToArray(), dotsTargets.Count, 1, Gamma);    
-		
-		//var pred = naiveRBFRegression(dotsInputs.ToArray(), dotsInputs.Count / 2, 2, predict, 1, 2, weights, dotsInputs.Count / 2, 2, 0.1f);
-		
+				
 		for (var i = -10; i <= 10; i ++)
 		{
 			for (var j = -10; j <= 10; j ++)
 			{
 				var result = 0f;
 				var valueToClassify = new float[] {i, j};
-				result = naiveRBFRegression(dotsInputs.ToArray(), dotsInputs.Count / 2, 2, valueToClassify, 1, 2, weights, dotsInputs.Count / 2, 1, Gamma);	
-				
-				Instantiate(original: result > 0 ? RedDot : BlueDot, position: new Vector3(x: i, y: -1.0f, z: j), rotation: Quaternion.identity);
+				result = naiveRBFRegression(dotsInputs.ToArray(), dotsInputs.Count / 2, 2, valueToClassify, 1, 2, weights, dotsInputs.Count / 2, 1, Gamma);
+				if (Regression)
+				{
+					Instantiate(original: PlanDot, position: new Vector3(x: i, y: result, z: j), rotation: Quaternion.identity);
+				}
+				else
+				{
+					Instantiate(original: result > 0 ? RedDot : BlueDot, position: new Vector3(x: i, y: -1.0f, z: j), rotation: Quaternion.identity);
+				}
 			}
 		}
 	}
